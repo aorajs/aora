532e38a89687b93bff6573b1ef889462e1a02da4
Zack <mwl@live.com>
2021-12-06 02:13:55 +0800

    feat: 更新文档

--- docs/.umirc.ts
+++ docs/.umirc.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'dumi';
+const repo = 'aora';
 
 export default defineConfig({
   title: 'Aora',
   mode: 'site',
+  outputPath: 'docs-dist',
+  base: `/${repo}/`,
+  publicPath: `/${repo}/`,
   // more config: https://d.umijs.org/config
 });
