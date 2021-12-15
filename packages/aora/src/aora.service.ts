70686e7c59a0c20f688c35ff1ff26352cc8881a7
Zack <mwl@live.com>
2021-12-15 20:35:08 +0800

    更新

--- packages/aora/src/aora.service.ts
+++ packages/aora/src/aora.service.ts
@@ -1,12 +1,24 @@
 import { Inject, Injectable, Scope } from '@nestjs/common'
 import { REQUEST } from '@nestjs/core'
-import type { Request , Response} from 'express'
+import type { Request, Response } from 'express'
 // @ts-ignore
 import { render } from 'aora/render'
+import { getCwd } from './index'
+import { join } from 'path'
+import { promises as fsp } from 'fs'
 
-type RenderOptions = Partial <{
+const cacache = require('cacache')
+const cachePath = join(getCwd(), './build/caches')
+const crypto = require('crypto');
+
+const md5 = (value: string) => {
+  return crypto.createHash('md5').update(value).digest('hex')
+}
+
+type RenderOptions = Partial<{
   ssr: Boolean
   stream: Boolean
+  cache: Boolean
 }>
 
 @Injectable({ scope: Scope.REQUEST })
@@ -16,9 +28,10 @@ export class AoraRenderService {
   constructor(@Inject(REQUEST) private readonly request: Request) {
     this.response = request.res!
     this.request = request
+    fsp.mkdir(cachePath, { recursive: true })
   }
 
-  async csr(data?: unknown, options: Exclude<RenderOptions, 'ssr'> = {}){ 
+  async csr(data?: unknown, options: Exclude<RenderOptions, 'ssr'> = {}) {
     console.log('csr', data)
     return this.render(data, {
       ...options,
@@ -26,7 +39,7 @@ export class AoraRenderService {
     })
   }
 
-  async ssr(data?: unknown, options: Exclude<RenderOptions, 'ssr'> = {}){ 
+  async ssr(data?: unknown, options: Exclude<RenderOptions, 'ssr'> = {}) {
     console.log('ssr', data)
     return this.render(data, {
       ...options,
@@ -36,12 +49,31 @@ export class AoraRenderService {
 
   async render(data?: unknown, options: RenderOptions = {}) {
     try {
+      const { cache = false, ...opts } = options
+      let cacheData
+      let cacheKey
+      if (cache && (cacheKey = md5(this.request.url))) {
+        try {
+          cacheData = await cacache.get(cachePath, cacheKey)?.data;
+          if (cacheData) {
+            if (Buffer.isBuffer(cacheData)) {
+              cacheData = cacheData.toString()
+            }
+            if (cacheData) return cacheData;
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
       const ctx = {
         request: this.request,
         response: this.response,
         data: data,
       };
-      const res = await render(ctx, options as any);
+      const res = await render(ctx, opts as any);
+      if (cache) {
+        cacache.put(cachePath, cacheKey, res);
+      }
       return res
     } catch (error) {
       this.response.status(500).send(error);
