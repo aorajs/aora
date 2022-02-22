f7745933e553b65797a43453f093467c0d90f71a
Loyep <mwl@live.com>
2022-02-22 20:34:08 +0800

    chore: delete files

--- examples/simple/.aorarc.ts
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'aora';
-
-export default defineConfig({
-  serverPort: 3001,
-});
--- examples/simple/pm2.config.js
+++ examples/simple/pm2.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'nest-react-ssr-app',
+      name: 'aora-example-simple',
       script: 'dist/main.js',
       exec_mode: 'cluster',
       max_memory_restart: '500M',
       env: {
         NODE_ENV: 'production',
-        BUILD_TOOL: process.env.BUILD_TOOL === 'vite' ? 'vite' : '',
       },
     },
   ],
