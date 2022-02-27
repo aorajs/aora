6ec833fd662ca518b73f2fcd3164ce82130c0b8c
Loyep <mwl@live.com>
2022-02-27 19:17:05 +0800

    chore:  update readme

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,6 @@ import type { Config } from '@jest/types';
 export default {
   testMatch: ['**/packages/*/src/**/*.test.ts'],
   transform: {
-    // alternatives:
-    // 1. @swc-node/jest
-    // 2. ts-jest
     '^.+\\.ts$': 'esbuild-jest',
   },
   testTimeout: 30000,
