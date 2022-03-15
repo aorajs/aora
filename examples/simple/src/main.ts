9280eba9a2f87316c4c341a53c49d142540b588f
Loyep <mwl@live.com>
2022-03-15 20:36:21 +0800

    chore: add `nestjs`

--- examples/simple/src/main.ts
+++ examples/simple/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { initialSSRDevProxy } from 'aora'
+import { initialSSRDevProxy } from 'aora';
 
 async function bootstrap(): Promise<NestExpressApplication> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.disable('x-powered-by');
   app.useStaticAssets('public', { maxAge: '1h', immutable: true });
+  await initialSSRDevProxy(app, {
     express: true
   })
+  await app.listen(3001);
   return app;
 }
 
---
+++ packages/nest/README.md
@@ -0,0 +1,3 @@
+# @aora/server
+
+See our website [aora](https://aorajs.com) for more information.
